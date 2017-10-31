fe4f11c669ea1f5c1cb78e5efdb0158a45b98eeb
Jaroslav Tulach <jaroslav.tulach@oracle.com>
2017-10-31 10:27:54 +0100

    Moving compilation and access to classloader into own class

---
+++ compiler/src/org.graalvm.compiler.javaout.test/src/org/graalvm/compiler/javaout/test/JavaCompilation.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright (c) 2013, 2015, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package org.graalvm.compiler.javaout.test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.reflect.Method;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Collections;
+import javax.tools.FileObject;
+import javax.tools.ForwardingJavaFileManager;
+import javax.tools.JavaCompiler;
+import javax.tools.JavaFileManager;
+import javax.tools.JavaFileObject;
+import javax.tools.SimpleJavaFileObject;
+import javax.tools.StandardJavaFileManager;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+final class JavaCompilation {
+
+    static byte[] assertCompile(String code) {
+        try {
+            JavaCompiler compiler = javax.tools.ToolProvider.getSystemJavaCompiler();
+
+            JavaFileObject file = new SimpleJavaFileObject(new URI("mem://Generated.java"), JavaFileObject.Kind.SOURCE) {
+                @Override
+                public String getName() {
+                    return "Generated.java";
+                }
+
+                @Override
+                public boolean isNameCompatible(String simpleName, JavaFileObject.Kind aKind) {
+                    if (!aKind.equals(getKind())) {
+                        return false;
+                    }
+                    return "Generated".equals(simpleName);
+                }
+
+                @Override
+                public CharSequence getCharContent(boolean ignoreEncodingErrors) throws IOException {
+                    return code;
+                }
+            };
+
+            final ByteArrayOutputStream os = new ByteArrayOutputStream();
+            JavaFileObject classFile = new SimpleJavaFileObject(new URI("mem://Generated.class"), JavaFileObject.Kind.CLASS) {
+                @Override
+                public OutputStream openOutputStream() throws IOException {
+                    return os;
+                }
+            };
+
+            JavaFileManager jfm = new ForwardingJavaFileManager<StandardJavaFileManager>(compiler.getStandardFileManager(null, null, null)) {
+                @Override
+                public JavaFileObject getJavaFileForOutput(JavaFileManager.Location location, String className, JavaFileObject.Kind kind, FileObject sibling) throws IOException {
+                    if ("Generated".equals(className)) {
+                        return classFile;
+                    }
+                    return super.getJavaFileForOutput(location, className, kind, sibling);
+                }
+            };
+
+            JavaCompiler.CompilationTask task = compiler.getTask(null, jfm, null, null, null, Collections.singleton(file));
+            boolean result = task.call();
+            assertTrue(code, result);
+            assertNotEquals("Some bytes written", 0, os.size());
+            return os.toByteArray();
+        } catch (URISyntaxException ex) {
+            throw new IllegalStateException(ex);
+        }
+    }
+
+    static Method assertMethod(String className, byte[] code, String methodName) {
+        try {
+            ClassLoader loader = new ClassLoader() {
+                @Override
+                protected Class<?> findClass(String name) throws ClassNotFoundException {
+                    if (className.equals(name)) {
+                        return defineClass(name, code, 0, code.length);
+                    }
+                    return null;
+                }
+            };
+            Class<?> clazz = loader.loadClass(className);
+            for (Method method : clazz.getMethods()) {
+                if (methodName.equals(method.getName())) {
+                    return method;
+                }
+            }
+            fail("No method " + methodName + " found");
+            return null;
+        } catch (ClassNotFoundException ex) {
+            throw new IllegalStateException(ex);
+        }
+    }
+
+}
--- compiler/src/org.graalvm.compiler.javaout.test/src/org/graalvm/compiler/javaout/test/JavaOutputTest.java
+++ compiler/src/org.graalvm.compiler.javaout.test/src/org/graalvm/compiler/javaout/test/JavaOutputTest.java
@@ -22,22 +22,11 @@
  */
 package org.graalvm.compiler.javaout.test;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.Collections;
 import java.util.Set;
-import javax.tools.FileObject;
-import javax.tools.ForwardingJavaFileManager;
-import javax.tools.JavaCompiler;
-import javax.tools.JavaFileManager;
-import javax.tools.JavaFileObject;
-import javax.tools.SimpleJavaFileObject;
-import javax.tools.StandardJavaFileManager;
 import jdk.vm.ci.meta.DeoptimizationReason;
 import jdk.vm.ci.meta.ResolvedJavaMethod;
 import org.graalvm.compiler.code.CompilationResult;
@@ -50,7 +39,6 @@ import org.graalvm.compiler.nodes.StructuredGraph;
 import org.graalvm.compiler.options.OptionValues;
 import org.graalvm.compiler.phases.OptimisticOptimizations;
 import org.junit.Assert;
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 
 public abstract class JavaOutputTest extends GraalCompilerTest {
@@ -137,90 +125,14 @@ public abstract class JavaOutputTest extends GraalCompilerTest {
         sb.append("\n}");
 
         code = sb.toString();
-        byte[] generated = assertCompile(code);
+        byte[] generated = JavaCompilation.assertCompile(code);
         assertNotNull("Bytes found", generated);
 
-        Method generatedMethod = assertMethod("Generated", generated, "test");
+        Method generatedMethod = JavaCompilation.assertMethod("Generated", generated, "test");
         try {
             Object actual = generatedMethod.invoke(receiver, args);
             Assert.assertEquals("The same result produced by:\n" + sb.toString(), expect.returnValue, actual);
         } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
-            throw new IllegalStateException(ex);
-        }
-    }
-
-    private byte[] assertCompile(String code) {
-        try {
-            JavaCompiler compiler = javax.tools.ToolProvider.getSystemJavaCompiler();
-
-            JavaFileObject file = new SimpleJavaFileObject(new URI("mem://Generated.java"), JavaFileObject.Kind.SOURCE) {
-                @Override
-                public String getName() {
-                    return "Generated.java";
-                }
-
-                @Override
-                public boolean isNameCompatible(String simpleName, JavaFileObject.Kind aKind) {
-                    if (!aKind.equals(getKind())) {
-                        return false;
-                    }
-                    return "Generated".equals(simpleName);
-                }
-
-                @Override
-                public CharSequence getCharContent(boolean ignoreEncodingErrors) throws IOException {
-                    return code;
-                }
-            };
-
-            final ByteArrayOutputStream os = new ByteArrayOutputStream();
-            JavaFileObject classFile = new SimpleJavaFileObject(new URI("mem://Generated.class"), JavaFileObject.Kind.CLASS) {
-                @Override
-                public OutputStream openOutputStream() throws IOException {
-                    return os;
-                }
-            };
-
-            JavaFileManager jfm = new ForwardingJavaFileManager<StandardJavaFileManager>(compiler.getStandardFileManager(null, null, null)) {
-                @Override
-                public JavaFileObject getJavaFileForOutput(JavaFileManager.Location location, String className, JavaFileObject.Kind kind, FileObject sibling) throws IOException {
-                    if ("Generated".equals(className)) {
-                        return classFile;
-                    }
-                    return super.getJavaFileForOutput(location, className, kind, sibling);
-                }
-            };
-
-            JavaCompiler.CompilationTask task = compiler.getTask(null, jfm, null, null, null, Collections.singleton(file));
-            boolean result = task.call();
-            assertTrue(code, result);
-            assertNotEquals("Some bytes written", 0, os.size());
-            return os.toByteArray();
-        } catch (URISyntaxException ex) {
-            throw new IllegalStateException(ex);
-        }
-    }
-
-    private Method assertMethod(String className, byte[] code, String methodName) {
-        try {
-            ClassLoader loader = new ClassLoader() {
-                @Override
-                protected Class<?> findClass(String name) throws ClassNotFoundException {
-                    if (className.equals(name)) {
-                        return defineClass(name, code, 0, code.length);
-                    }
-                    return null;
-                }
-            };
-            Class<?> clazz = loader.loadClass(className);
-            for (Method method : clazz.getMethods()) {
-                if (methodName.equals(method.getName())) {
-                    return method;
-                }
-            }
-            fail("No method " + methodName + " found");
-            return null;
-        } catch (ClassNotFoundException ex) {
             throw new IllegalStateException(ex);
         }
     }
