abe66a53afca8f9c666bcbc3f4f83f562843da0c
Jaroslav Tulach <jaroslav.tulach@oracle.com>
2017-10-31 09:07:28 +0100

    Infastructure to verify generated Java code behaves the same as original and a simplistic implementation to verify it works for 13 % 8

---
+++ compiler/src/org.graalvm.compiler.javaout.test/src/org/graalvm/compiler/javaout/test/JavaOutput.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright (c) 2013, 2015, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package org.graalvm.compiler.javaout.test;
+
+import java.io.IOException;
+import java.util.Iterator;
+import org.graalvm.compiler.graph.Node;
+import org.graalvm.compiler.graph.iterators.NodeIterable;
+import org.graalvm.compiler.nodes.BinaryOpLogicNode;
+import org.graalvm.compiler.nodes.ConstantNode;
+import org.graalvm.compiler.nodes.ParameterNode;
+import org.graalvm.compiler.nodes.ReturnNode;
+import org.graalvm.compiler.nodes.StartNode;
+import org.graalvm.compiler.nodes.StructuredGraph;
+import org.graalvm.compiler.nodes.calc.BinaryNode;
+import org.graalvm.compiler.nodes.calc.ConditionalNode;
+import org.graalvm.compiler.nodes.calc.NegateNode;
+import org.graalvm.compiler.nodes.calc.UnaryNode;
+
+public final class JavaOutput {
+    private final StructuredGraph g;
+
+    public JavaOutput(StructuredGraph g) {
+        this.g = g;
+    }
+
+    public void generate(Appendable out, String sep) throws IOException {
+        dump(out, g.start(), sep);
+    }
+
+    private static void dump(Appendable out, Node at, String sep) throws IOException {
+        final String moreSep = sep + "  ";
+        if (at instanceof ParameterNode) {
+            StructuredGraph g = (StructuredGraph) at.graph();
+            String paramName = g.getMethods().get(0).getParameters()[((ParameterNode) at).index()].getName();
+            out.append(paramName);
+            return;
+        }
+        if (at instanceof ConstantNode) {
+            String value = ((ConstantNode) at).getValue().toValueString();
+            out.append(value);
+            return;
+        }
+        if (at instanceof BinaryOpLogicNode || at instanceof BinaryNode) {
+            NodeIterable<Node> two = at.inputs();
+            assert 2 == two.count();
+            out.append("(");
+            Iterator<Node> twoIt = two.iterator();
+            dump(out, twoIt.next(), "");
+            out.append(" ").append(at.getNodeClass().shortName()).append(" ");
+            dump(out, twoIt.next(), "");
+            out.append(")");
+            return;
+        }
+        if (at instanceof NegateNode) {
+            out.append("-");
+            dump(out, ((UnaryNode) at).getValue(), "");
+            return;
+        }
+        if (at instanceof ConditionalNode) {
+            out.append("(");
+            dump(out, ((ConditionalNode) at).condition(), "");
+            out.append(" ? ");
+            dump(out, ((ConditionalNode) at).trueValue(), "");
+            out.append(" : ");
+            dump(out, ((ConditionalNode) at).falseValue(), "");
+            out.append(")");
+            return;
+        }
+        if (at instanceof ReturnNode) {
+            out.append(sep).append("return ");
+            dump(out, ((ReturnNode) at).result(), "");
+            out.append(";");
+            return;
+        }
+        if (at instanceof StartNode) {
+            for (Node next : at.cfgSuccessors()) {
+                dump(out, next, sep);
+            }
+            return;
+        }
+
+        for (Node in : at.inputs()) {
+            out.append(sep).append(" in ").append(in.toString()).append("\n");
+            dump(out, in, moreSep);
+            out.append("\n");
+        }
+        out.append(sep).append(at.toString()).append("\n");
+        System.err.println("   p: " + at.cfgPredecessors().toString());
+        System.err.println("   n: " + at.cfgSuccessors().toString());
+        for (Node next : at.cfgSuccessors()) {
+            dump(out, next, moreSep);
+        }
+    }
+}
--- compiler/src/org.graalvm.compiler.javaout.test/src/org/graalvm/compiler/javaout/test/JavaOutputTest.java
+++ compiler/src/org.graalvm.compiler.javaout.test/src/org/graalvm/compiler/javaout/test/JavaOutputTest.java
@@ -1,7 +1,43 @@
+/*
+ * Copyright (c) 2013, 2015, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
 package org.graalvm.compiler.javaout.test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Collections;
 import java.util.Set;
+import javax.tools.FileObject;
+import javax.tools.ForwardingJavaFileManager;
+import javax.tools.JavaCompiler;
+import javax.tools.JavaFileManager;
+import javax.tools.JavaFileObject;
+import javax.tools.SimpleJavaFileObject;
+import javax.tools.StandardJavaFileManager;
 import jdk.vm.ci.meta.DeoptimizationReason;
 import jdk.vm.ci.meta.ResolvedJavaMethod;
 import org.graalvm.compiler.code.CompilationResult;
@@ -14,6 +50,8 @@ import org.graalvm.compiler.lir.asm.CompilationResultBuilderFactory;
 import org.graalvm.compiler.nodes.StructuredGraph;
 import org.graalvm.compiler.options.OptionValues;
 import org.graalvm.compiler.phases.OptimisticOptimizations;
+import org.junit.Assert;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 
 public abstract class JavaOutputTest extends GraalCompilerTest {
@@ -75,5 +113,114 @@ public abstract class JavaOutputTest extends GraalCompilerTest {
         executeActualCheckDeopt(options, method, shouldNotDeopt, receiver, args);
         assertNotNull("Optimized graph generated", optimizedGraph);
 
+        JavaOutput code = new JavaOutput(optimizedGraph);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("\npublic class Generated {\n");
+        ResolvedJavaMethod m = optimizedGraph.getMethods().get(0);
+        sb.append("  public static ").append(m.getSignature().getReturnType(m.getDeclaringClass()).toJavaName());
+        sb.append(" test(");
+        for (int i = 0; i < m.getParameters().length; i++) {
+            if (i > 0) {
+                sb.append(", ");
+            }
+            final ResolvedJavaMethod.Parameter p = m.getParameters()[i];
+            sb.append(p.getType().toJavaName()).append(" ").append(p.getName());
+        }
+        sb.append(") {\n");
+        try {
+            code.generate(sb, "    ");
+        } catch (IOException ex) {
+            throw new IllegalStateException(ex);
+        }
+        sb.append("\n  }");
+        sb.append("\n}");
+
+        byte[] generated = assertCompile(sb.toString());
+        assertNotNull("Bytes found", generated);
+
+        Method generatedMethod = assertMethod("Generated", generated, "test");
+        try {
+            Object actual = generatedMethod.invoke(receiver, args);
+            Assert.assertEquals("The same result produced by:\n" + sb.toString(), expect.returnValue, actual);
+        } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
+            throw new IllegalStateException(ex);
+        }
+    }
+
+    private byte[] assertCompile(String code) {
+        try {
+            JavaCompiler compiler = javax.tools.ToolProvider.getSystemJavaCompiler();
+
+            JavaFileObject file = new SimpleJavaFileObject(new URI("mem://Generated.java"), JavaFileObject.Kind.SOURCE) {
+                @Override
+                public String getName() {
+                    return "Generated.java";
+                }
+
+                @Override
+                public boolean isNameCompatible(String simpleName, JavaFileObject.Kind kind) {
+                    if (!kind.equals(getKind())) {
+                        return false;
+                    }
+                    return "Generated".equals(simpleName);
+                }
+
+                @Override
+                public CharSequence getCharContent(boolean ignoreEncodingErrors) throws IOException {
+                    return code;
+                }
+            };
+
+            final ByteArrayOutputStream os = new ByteArrayOutputStream();
+            JavaFileObject classFile = new SimpleJavaFileObject(new URI("mem://Generated.class"), JavaFileObject.Kind.CLASS) {
+                @Override
+                public OutputStream openOutputStream() throws IOException {
+                    return os;
+                }
+            };
+
+            JavaFileManager jfm = new ForwardingJavaFileManager<StandardJavaFileManager>(compiler.getStandardFileManager(null, null, null)) {
+                @Override
+                public JavaFileObject getJavaFileForOutput(JavaFileManager.Location location, String className, JavaFileObject.Kind kind, FileObject sibling) throws IOException {
+                    if ("Generated".equals(className)) {
+                        return classFile;
+                    }
+                    return super.getJavaFileForOutput(location, className, kind, sibling);
+                }
+            };
+
+            JavaCompiler.CompilationTask task = compiler.getTask(null, jfm, null, null, null, Collections.singleton(file));
+            boolean result = task.call();
+            assertTrue(code, result);
+            assertNotEquals("Some bytes written", 0, os.size());
+            return os.toByteArray();
+        } catch (URISyntaxException ex) {
+            throw new IllegalStateException(ex);
+        }
+    }
+
+    private Method assertMethod(String className, byte[] code, String methodName) {
+        try {
+            ClassLoader loader = new ClassLoader() {
+                @Override
+                protected Class<?> findClass(String name) throws ClassNotFoundException {
+                    if (className.equals(name)) {
+                        return defineClass(name, code, 0, code.length);
+                    }
+                    return null;
+                }
+            };
+            Class<?> clazz = loader.loadClass(className);
+            for (Method method : clazz.getMethods()) {
+                if (methodName.equals(method.getName())) {
+                    return method;
+                }
+            }
+            fail("No method " + methodName + " found");
+            return null;
+        } catch (ClassNotFoundException ex) {
+            throw new IllegalStateException(ex);
+        }
     }
 }
